ed423c150e0bf4ef63ab3369b75e49fa78bfab6f
Alexander Belles <aub1461@psu.edu>
2020-01-15 11:37:58 -0500

    Add most used constants

---
+++ thick_rt/constants.py
@@ -0,0 +1,41 @@
+
+
+# Author: Alex Belles
+# Last updated: Jan 15, 2020
+
+# This script defines constants that will be used during Astro 530. The
+# constants will be in CGS units unless otherwise noted. Constants will be in
+# all upper case letters. In the case where a constant has two different units
+# (for example planck's constant in erg*s or eV*s), the name of the variable
+# will have the units appened after an underscore. Most of theseconstants come
+# from the following webpage:
+# http://www.astro.wisc.edu/~dolan/constants.html
+
+# Physical constants
+
+SPEEDOFLIGHT = 2.99792458e10           # cm/s
+PLANCK_ERG = 6.6260755e-27             # erg s
+PLANCK_EV = 4.13566766e-15             # eV s
+GRAV_CONST = 6.67259e-8                # cm^3/g/s^2
+ELEC_CHARGE = 4.8032068e-10            # esu or statcolumbs
+ELEC_MASS = 9.1093897e-28              # grams
+PROTON_MASS = 1.6726231e-24            # grams
+NEUTRON_MASS = 1.6749286e-24           # grams
+HYDROGEN_MASS = 1.6733e-24             # grams
+AMU = 1.6605402e-24                    # grams
+AVAGADRO_NUM = 6.0221367e23            # dimensionless
+BOLTZMANN_ERG = 1.380658e-16           # erg/K
+BOLTZMANN_EV = 8.61733034e-5           # eV/K
+EV_TO_ERG = 1.6021772e-12              # 1 eV = 1 EV_TO_ERG
+RAD_DENS_CONST = 7.5646e-15            # a, erg/cm^3/K^4
+STEFAN_BOLTZMANN = 5.67051e-5          # \sigma, erg/cm^2/K^4/s
+
+# Astronomical constants
+
+AU = 1.496e13                          # cm
+PARSEC = 3.086e18                      # cm
+LIGHTYEAR = 9.463e17                   # cm
+SOLAR_MASS = 1.99e33                   # gram
+SOLAR_RADIUS = 6.96e10                 # cm
+SOLAR_LUM = 3.9e33                     # erg/s
+SOLAR_EFF_TEMP = 5.780e3               # K
