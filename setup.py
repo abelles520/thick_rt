791660ac8f3d649c99b76c232960f3d4af2e7073
Alexander Belles <aub1461@psu.edu>
2020-01-15 09:50:09 -0500

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuprools import setup
+
+setup(name='thick_rt',
+      version='0.1',
+      description='Optically thick radiative transfer for Astro 530: Stellar Atmospheres',
+      url='http://github.com/abelles520/thick_rt',
+      author='Alex Belles',
+      author_email='aub1461@psu.edu',
+      license='MIT',
+      packages=['thick_rt'],
+      zip_safe=False)
