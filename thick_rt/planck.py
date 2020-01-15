adcc018034d1faeaf71a15dc2fee79d669846b7e
Alexander Belles <aub1461@psu.edu>
2020-01-15 11:45:29 -0500

    Add planck function in freq and wave forms

---
+++ thick_rt/planck.py
@@ -0,0 +1,21 @@
+
+
+# Author: Alex Belles
+# Last updated: Jan 15, 2020
+
+# This script will have functions for the planck function. It is very possible
+# that this script will get renamed at some point.
+
+
+
+def planck_freq(nu, T):
+    """
+    Planck law function in terms of frequency
+    """
+    return (2*PLANCK_ERG*nu**3/SPEEDOFLIGHT**2)/(exp((PLANCK_ERG*nu)/(BOLTZMANN_ERG*T))-1)
+
+def planck_wave(wave, T):
+    """
+    Planck law function in terms of wavelength
+    """
+    return (2*PLANCK_ERG*SPEEDOFLIGHT**2/wave**5)/(exp((PLANCK_ERG*SPEEDOFLIGHT)/(wave*BOLTZMANN_ERG*T))-1)
