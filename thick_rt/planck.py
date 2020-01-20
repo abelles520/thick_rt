1cd43adb9361bab7fe20517f32c9e38265bef4b9
Alexander Belles <aub1461@psu.edu>
2020-01-20 09:23:45 -0500

    Add wavenumber planck function

--- thick_rt/planck.py
+++ thick_rt/planck.py
@@ -1,7 +1,7 @@
 
 
 # Author: Alex Belles
-# Last updated: Jan 15, 2020
+# Last updated: Jan 20, 2020
 
 # This script will have functions for the planck function. It is very possible
 # that this script will get renamed at some point.
@@ -29,3 +29,27 @@ def planck_wave(wave, T):
     returns in units of erg/s/cm^2/A
     """
     return (1e8)**4*(2*PLANCK_ERG*SPEEDOFLIGHT**2/wave**5)/(np.exp((1e8*PLANCK_ERG*SPEEDOFLIGHT)/(wave*BOLTZMANN_ERG*T))-1)
+
+
+def planck_wavenum(wavenum, T):
+    """
+    Planck law function in terms of wavenumber where wavenumber is defined to be
+    \tilde{\nu} = 1/\lambda
+
+    typically has units of 1/um
+
+    Inputs:
+    wavenum - wavenumber in 1/um
+    T - temperature in K
+
+    returns in units of ergs/s/cm^2/Hz
+    """
+
+    wave = 1/wavenum*1e4    # convert to wavelength in angstroms
+
+    tmp = planck_wave(wave, T)
+
+    # convert to frequency units so multiply by wave^2/c
+    # don't forget 1e8 factor to convert speed of light to angstrom/s
+
+    return tmp*wave**2/(1e8*SPEEDOFLIGHT)
