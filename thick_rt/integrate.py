d1937faf2da9c7131134f237fc4518aae80f328c
aub1461 <aub1461@psu.edu>
2020-02-05 13:57:31 -0500

    add function for log spacing integration

--- thick_rt/integrate.py
+++ thick_rt/integrate.py
@@ -1,8 +1,10 @@
 
 # Author: Alex Belles
-# Last updated: Jan 20, 2020
+# Last updated: Feb 5, 2020
 
 # This script contains a basic trapezodial rule integrator
+
+import numpy as np
 
 def trapezoidal(f, a, b, n):
     """
@@ -26,3 +28,21 @@ def trapezoidal(f, a, b, n):
         s += f(a + i*h)
     s += f(b)/2.0
     return s*h
+
+def trap_log(f, a, b, n):
+    """
+    This function uses the trapezoidal rule to integrate basic functions
+    This function uses logarithmic spacing
+
+    Inputs:
+    f - a python function to be integrated
+    a - lower bound
+    b - upper bound
+    n - number of points to use in numeric integration
+
+    Outputs:
+    A single number cooresponding to the function integrated over the given bounds
+    """
+    # See this stack overflow:  https://stackoverflow.com/questions/21146540/trapezoidal-rule-in-python
+
+    return trapezoidal(lambda x: f(np.exp(x))*np.exp(x), np.log(a), np.log(b), n)
