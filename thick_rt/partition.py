02f3be6db8826210d476375bfb9ec6e07013fabd
Alexander Belles <aub1461@psu.edu>
2020-02-25 09:24:38 -0500

    fix path typo

--- thick_rt/partition.py
+++ thick_rt/partition.py
@@ -16,7 +16,7 @@ def partition(ion_name, T):
     """
     if ion_name in ['H+', 'H-']:
         return 1.0
-    df = pd.read_csv('../aux_files/partit.txt', delim_whitespace=True, header=None,  na_values='-')
+    df = pd.read_csv('aux_files/partit.txt', delim_whitespace=True, header=None,  na_values='-')
     df.columns = ['name', 0.2, 0.4, 0.6 ,0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 'log g0']
     
     row = df[df['name']==ion_name]
